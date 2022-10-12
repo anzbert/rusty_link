a13f79938f283ec481feb8cd7e87400a5c1da7e0
anzbert <anzbert@gmail.com>
2022-10-12 22:23:50 +0800

    input fn for example

--- examples/link_hut.rs
+++ examples/link_hut.rs
@@ -1,43 +1,82 @@
+use crossterm::event::{poll, read, Event, KeyCode};
 use rusty_link::{AblLink, SessionState};
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::time::Duration;
 
-fn main() {
-    // Setup Ctrl-C Handler:
-    let running = std::sync::Arc::new(AtomicBool::new(true));
-    let r = running.clone();
-    ctrlc::set_handler(move || {
-        r.store(false, Ordering::SeqCst);
-    })
-    .expect("Error setting Ctrl-C handler");
+pub struct State {
+    pub link: AblLink,
+    pub session_state: SessionState,
+    pub running: bool,
+    pub quantum: f64,
+}
 
-    // Setup Link:
-    let quantum = 4.0;
-    let mut link = AblLink::new(120.0);
-    let mut session_state = SessionState::new();
-    link.enable(true);
-    link.enable_start_stop_sync(true);
-
-    // Callback Example:
-    link.set_start_stop_callback(|value: bool| {
-        println!("is_playing: {} with quantum: {}", value, quantum)
-    });
-
-    // Main Loop wrapped in Ctrl-C Handler:
-    while running.load(Ordering::SeqCst) {
-        link.capture_app_session_state(&mut session_state);
-
-        let time = link.clock_micros();
-        let tempo = session_state.tempo();
-        let playing = session_state.is_playing();
-        let beat = session_state.beat_at_time(time, quantum);
-        println!(
-            "playing={}, quantum={}, clock={}, tempo={}, beat={}",
-            playing, quantum, time, tempo, beat
-        );
-        std::thread::sleep(std::time::Duration::from_millis(100));
+impl State {
+    pub fn new() -> Self {
+        Self {
+            link: AblLink::new(100.),
+            session_state: SessionState::new(),
+            running: true,
+            quantum: 4.,
+        }
     }
 
-    // Exit Routine:
-    println!("Leaving Link session");
-    link.enable(false);
+    pub fn update_state(&mut self) {
+        self.link.capture_app_session_state(&mut self.session_state);
+    }
+}
+
+fn input(state: &mut State) -> crossterm::Result<()> {
+    // `poll()` waits for an `Event` for a given time period
+    if poll(Duration::from_millis(50))? {
+        // It's guaranteed that the `read()` won't block when the `poll()`
+        // function returns `true`
+        if let Event::Key(event) = read()? {
+            state.update_state();
+            let tempo = state.session_state.tempo();
+            let time_stamp = state.link.clock_micros();
+            let enabled = state.link.is_enabled();
+
+            match event.code {
+                KeyCode::Char('q') => state.running = false,
+                KeyCode::Char('a') => state.link.enable(!enabled),
+                KeyCode::Char('w') => state.session_state.set_tempo(tempo - 1., time_stamp),
+                KeyCode::Char('e') => state.session_state.set_tempo(tempo + 1., time_stamp),
+                KeyCode::Char('r') => state.quantum -= 1.,
+                KeyCode::Char('t') => state.quantum += 1.,
+                KeyCode::Char('s') => state
+                    .link
+                    .enable_start_stop_sync(!state.link.is_start_stop_sync_enabled()),
+                KeyCode::Char(' ') => {
+                    if state.session_state.is_playing() {
+                        state.session_state.set_is_playing(false, time_stamp as u64);
+                    } else {
+                        state.session_state.set_is_playing_and_request_beat_at_time(
+                            true,
+                            time_stamp as u64,
+                            0.,
+                            state.quantum,
+                        )
+                    }
+                }
+                _ => {}
+            }
+            state.link.commit_app_session_state(&state.session_state);
+        }
+    }
+
+    Ok(())
+}
+
+fn main() {
+    let mut state = State::new();
+
+    // Callback Example:
+    // state.link.set_start_stop_callback(|value: bool| {
+    //     println!("is_playing: {} with quantum: {}", value, quantum)
+    // });
+
+    '_main_loop: while state.running {
+        input(&mut state).expect("Input Err");
+    }
+
+    state.link.enable(false);
 }
