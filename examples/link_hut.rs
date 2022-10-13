49e23e9a41cb8c7c47f19000227e0329da06fa76
anzbert <anzbert@gmail.com>
2022-10-13 08:33:47 +0800

    new example

--- examples/link_hut.rs
+++ examples/link_hut.rs
@@ -1,3 +1,6 @@
+// This example is a Rust port of the Ableton Link 'link_hut' example written in C.
+// Original: https://github.com/Ableton/link/blob/master/extensions/abl_link/examples/link_hut/main.c
+
 use crossterm::{
     cursor,
     event::{poll, read, Event, KeyCode},
@@ -28,64 +31,66 @@ impl State {
         }
     }
 
-    pub fn update_state(&mut self) {
+    pub fn update_app_state(&mut self) {
         self.link.capture_app_session_state(&mut self.session_state);
     }
 }
 
 fn print_state(state: &mut State) {
-    state.update_state();
+    state.update_app_state();
 
     let time = state.link.clock_micros();
     let enabled = match state.link.is_enabled() {
         true => "yes",
-        false => "no",
+        false => "no ",
     }
     .to_string();
     let num_peers = state.link.num_peers();
     let start_stop = match state.link.is_start_stop_sync_enabled() {
         true => "yes",
-        false => "no",
+        false => "no ",
     };
     let playing = match state.session_state.is_playing() {
-        true => "yes",
-        false => "no",
+        true => "[playing]",
+        false => "[stopped]",
     };
     let tempo = state.session_state.tempo();
     let beats = state.session_state.beat_at_time(time, state.quantum);
     let phase = state.session_state.phase_at_time(time, state.quantum);
     let mut metro = String::with_capacity(state.quantum as usize);
     for i in 0..state.quantum as usize {
-        if i < phase as usize {
-            metro.push('X');
-        } else {
+        if i > phase as usize {
             metro.push('O');
+        } else {
+            metro.push('X');
         }
     }
 
     let mut stdout = stdout();
     queue!(
         stdout,
-        Print(format!("{} | ", enabled)),
-        Print(format!("{} | ", num_peers)),
-        Print(format!("{} | ", state.quantum)),
-        Print(format!("{} {} | ", start_stop, playing)),
-        Print(format!("{} | ", tempo)),
-        Print(format!("{} | ", beats)),
+        cursor::SavePosition,
+        terminal::Clear(terminal::ClearType::FromCursorDown),
+        Print(format!("{:<7} | ", enabled)),
+        Print(format!("{:<9} | ", num_peers)),
+        Print(format!("{:<7} | ", state.quantum.trunc())),
+        Print(format!("{:<3}   {:<9} | ", start_stop, playing)),
+        Print(format!("{:<7.2} | ", tempo)),
+        Print(format!("{:<8.2} | ", beats)),
         Print(format!("{}", metro)),
-        cursor::MoveUp(1),
+        cursor::RestorePosition,
     )
     .unwrap();
     stdout.flush().unwrap();
 }
 
-fn input(state: &mut State) -> crossterm::Result<()> {
+fn poll_input(state: &mut State) -> crossterm::Result<()> {
     // `poll()` waits for an `Event` for a given time period
     if poll(Duration::from_millis(50))? {
         // It's guaranteed that the `read()` won't block when the `poll()`
         // function returns `true`
         if let Event::Key(event) = read()? {
-            state.update_state();
+            state.update_app_state();
             let tempo = state.session_state.tempo();
             let time_stamp = state.link.clock_micros();
             let enabled = state.link.is_enabled();
@@ -121,24 +126,32 @@ fn input(state: &mut State) -> crossterm::Result<()> {
             state.link.commit_app_session_state(&state.session_state);
         }
     }
-
     Ok(())
 }
 
 fn main() {
+    println!("\n\n < L I N K  H U T >\n");
+
+    println!("usage:");
+    println!("  enable / disable Link: a");
+    println!("  start / stop: space");
+    println!("  decrease / increase tempo: w / e");
+    println!("  decrease / increase quantum: r / t");
+    println!("  enable / disable start stop sync: s");
+    println!("  quit: q");
+
+    println!("\nenabled | num peers | quantum | start stop sync | tempo   | beats    | metro");
+
     let mut state = State::new();
+
     terminal::enable_raw_mode().unwrap();
 
-    // Callback Example:
-    // state.link.set_start_stop_callback(|value: bool| {
-    //     println!("is_playing: {} with quantum: {}", value, quantum)
-    // });
-
     '_main_loop: while state.running {
-        input(&mut state).expect("Input Err");
+        poll_input(&mut state).expect("Input Fn Error");
         print_state(&mut state);
     }
 
-    state.link.enable(false);
     terminal::disable_raw_mode().unwrap();
+
+    state.link.enable(false);
 }
